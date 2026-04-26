74411b8c078e9c8921294073b0e380f865bc8036
azisbr <azisbr23.24@gmail.com>
2026-04-26 17:05:18 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,245 @@
+# config.py
+# ==================================================
+# THORYXFF BIO INJECTOR - CONFIGURATION
+# Edit these values to customize your site
+# ==================================================
+
+SITE_CONFIG = {
+    # Site Identity
+    "site_name": "THORYXFF",
+    "site_logo_emoji": "⚡",
+    
+    # Free Fire Version
+    "freefire_version": "OB53",
+    
+    # Social Links (for popup)
+    "youtube_link": "https://www.youtube.com/@GamesOfThory",
+    "instagram_link": "https://www.instagram.com/THORYXFF",
+    
+    # Popup Settings
+    "popup_title": "⚡ JOIN THE COMMUNITY ⚡",
+    "popup_message": "Subscribe to YouTube & Follow on Instagram to unlock the Bio Injector!",
+    
+    # Bio Settings
+    "bio_char_limit": 300,
+    "default_region": "IND",
+    
+    # Footer
+    "footer_text": "THORYXFF • Neon Bio Injector",   # ← comma is correct here!
+    
+    # Video Tutorial Links
+    "howto_video_link": "https://youtu.be/VwWdm9UFZRM",
+    "create_own_video_link": "https://github.com/thory99/FF-Long-Bio",   # ← last line, no comma
+
+
+    "templates": [
+  {
+    "name": "THORY SPECIAL",
+    "bio": "[B][C][I][FEA100]ㅤ ╭─⎾ Thory ⏋[FFFFFF] ↓[FF1493]IG THORYXFF[FFFFFF]↓\n╭┘▣│ᴀɪ ᴡᴏʀʟᴅ│[78CDFF]YT - GAMESOFTHORY\n[7AC279][U]└◎─┴─◎─◎[/U]┘[F5BE63]EMOTE BOT KE LIYE YT CHECK"
+  },
+  {
+    "name": "ATTITUDE SHAYARI",
+    "bio": "[b][F5F5F5]Hᴜᴍ ᴛᴜᴍʜᴇ [ffff00]Mᴀʀᴇɴɢᴇ [F5F5F5]ᴀᴜʀ [ff0000]ᴢᴀʀᴜʀ [ffff00]ᴍᴀʀᴇɴɢᴇ… [ffffff]ʟᴇᴋɪɴ [1fff06]Bᴀɴᴅᴏᴏᴋ [ff00ff]╾═デ╦︻ [F5F5F5]ʙʜɪ ʜᴀᴍᴀʀɪ ʜᴏɢᴀ,\n[FF4DFF]Gᴏʟɪ [F5F5F5]ʙʜɪ ʜᴀᴍᴀʀɪ ʜᴏɢᴀ… ᴀᴜʀ ᴡᴏʜ [FFA726]Wᴀǫᴛ [F5F5F5]ʙʜɪ [00ffff]ʜᴀᴍᴀʀᴀ [ffffff]ʜᴏɢᴀ"
+  },
+  {
+    "name": "GLADIATOR",
+    "bio": "[B][C][FFD700]◤◢◣◥ GLADIATOR ◤◢◣◥\n[FF69B4]║ MAIDAN E JANG MEIN KHATARNAAK HUUN ║\n[00FF7F]◣◥◤◢ NO RETREAT SIRF AAGE BADHNA ◣◥◤◢"
+  },
+  {
+    "name": "NEON PHANTOM",
+    "bio": "[I][00FFFF]┌─┐ N E O N P H A N T O M ┌─┐\n[FF00FF]│ RAT MEIN DIKHU NAHI SIRF MAARU │\n[00FFFF]└─┘ JAISE BHOOT WAISE GAME KHATAM └─┘"
+  },
+  {
+    "name": "ROYAL BEAST",
+    "bio": "[B][C][FF1493]╱╲╱╲ ROYAL BEAST ╲╱╲╱\n[FFD700]│ SHER HAI TU TOH KYA MAIN TOH KALKI HUUN │\n[FF4500]╲╱╲╱ JUNGLE KA BADSHAH SIRF MAIN HUUN ╱╲╱╲"
+  },
+  {
+    "name": "ELECTRIC SOUL",
+    "bio": "[I][00FF00]▁▂▃▄ CHARGE HOJA ELECTRIC SOUL ▄▃▂▁\n[FFFF00]BOLT SE BHI FAST HUUN CURRENCY SE BHI EXPENSIVE\n[00FF00]▔▁▂ CURRENT LAGEGI TERI JALEGI BAAT SAMJH ▂▁▔"
+  },
+  {
+    "name": "ANGEL OF DEATH",
+    "bio": "[C][FFFFFF]♡ ── ANGEL OF DEATH ── ♡\n[FF69B4]PARI NAHI HUUN MAIN TOH KATIL HUUN SACHCHI\n[FFFFFF]♡ ── KHATAM KARKE JATI HUUN MUSKURATE HUYE ── ♡"
+  },
+  {
+    "name": "CYBER KING",
+    "bio": "[B][00FFFF]⌈⌉⌈⌉ CYBER KING ⌉⌈⌉⌈\n[FF00FF]│ 01001001 00100000 01000001 01001101 │\n[00FFFF]⌊⌋⌊⌋ CODE MEIN LIKHA HAI TERA GAME OVER ⌋⌊⌋⌊"
+  },
+  {
+    "name": "FROZEN FIRE",
+    "bio": "[I][87CEEB]❄️━━ FROZEN FIRE ━━❄️\n[FF4500]BARF MEIN AAG LAGA DETA HUUN MAIN DEKHLE\n[87CEEB]━━ THANDA DIL LEKIN GARAM KHEL KHABARDAAR ━━"
+  },
+  {
+    "name": "PINK PANTHER",
+    "bio": "[B][C][FF69B4]╭━━━━━━━━━━━━━━━━╮\n[FFB6C1]│ GULAABI RANG LEKIN KATIL KHAAS │\n[FF69B4]╰━━━━━━━━━━━━━━━━╯"
+  },
+  {
+    "name": "GOLDEN EAGLE",
+    "bio": "[I][FFD700]⎯⎯ GOLDEN EAGLE ⎯⎯\n[FF8C00]UPAR SE NAZAR AATI HUUN NEECHE SE MARETI HUUN\n[FFD700]⎯⎯ AANKH SE DEKHA NAHI DUSHMAN GAYA SAMJH ⎯⎯"
+  },
+  {
+    "name": "OCEAN KING",
+    "bio": "[B][C][00BFFF]~~~~~~~~~~~~~~ OCEAN KING ~~~~~~~~~~~~~~\n[7FFFD4]LEHERO PE SWAR HAI PAANI MEIN GHAR HAI MERA\n[00BFFF]~~~~~~~~~~~~~~ DOOBEGA JO SAMNE AAYEGA ~~~~~~~~~~~~~~"
+  },
+  {
+    "name": "SUNSET RIDER",
+    "bio": "[I][FF7F50]╱╲ SUNSET RIDER ╱╲\n[FFD700]SHAAM DHALNE SE PEHLE KAM KHATAM KARDU\n[FF7F50]╲╱ DHOOP MEIN BHI AAG JAISI RIDER HUUN ╲╱"
+  },
+  {
+    "name": "STARDUST",
+    "bio": "[C][FFD700]✧･ﾟ STARDUST DESTROYER ･ﾟ✧\n[FF69B4]TARO SE BAAT KARTA HUUN DUSHMANO SE NAHI\n[FFD700]✧ GIRATE HUUN SABKO JAISE AASMAN SE TARAZU ✧"
+  },
+  {
+    "name": "MAGICAL GHOST",
+    "bio": "[B][I][BA55D3]┌─⎾ MAGICAL GHOST ⏋─┐\n[DA70D6]│ JADUI BHOOT HUUN DIKHTA NAHI DHAMAKA DETA │\n[BA55D3]└─⎾ ENEMY SOTA RAHEGA MAIN KHELTA RAHUNGA ─┘"
+  },
+  {
+    "name": "DESERT WOLF",
+    "bio": "[I][FFA500]⌜⌝ DESERT WOLF ⌜⌝\n[FF8C00]│ REGISTAN KA SHER GARMI MEIN BHI DANGER │\n[FFA500]⌞⌟ PYAASA NAHI PYACHAHUUN MAUT KO ┘"
+  },
+  {
+    "name": "SKY HUNTER",
+    "bio": "[B][C][87CEFA]⌈⌈⌈ SKY HUNTER ⌋⌋⌋\n[00BFFF]HAWA MEIN UDTA HUUN ZAMIN SE MARTAA HUUN\n[87CEFA]⌊⌊⌊ NEECHE AAKE DIKHA PHIR BAAT KARENGE ⌈⌈⌈"
+  },
+  {
+    "name": "LAVA KING",
+    "bio": "[I][FF4500]▀▄▀ LAVA KING ▀▄▀\n[FF6347]PIGHLA HUWA KHATARNAAK GARMI SE DARTA NAHI\n[FF4500]▄▀▄ JO GARMI SEHEN NAHI KARE WOH BHAAG JAATAAAA ▄▀▄"
+  },
+  {
+    "name": "MOONLIGHT",
+    "bio": "[B][C][E0FFFF]~~~~ MOONLIGHT ASSASSIN ~~~~\n[FF69B4]RAAT KI RANI HUUN ANDHERO MEIN KHATARNAAK\n[E0FFFF]~~~~ CHANDNI MEIN NACHU LASHO KE SAATH BAS ~~~~"
+  },
+  {
+    "name": "RED VIPER",
+    "bio": "[I][DC143C]╭━╮ RED VIPER ╭━╮\n[FF1493]│ KATIL SAANP HUUN DASUNGA TOH GAME OVER │\n[DC143C]╰━╯ ZAHAR CHADHA KAR CHALA HUUN DEKH SAMBHAL KE ╰━╯"
+  },
+  {
+    "name": "GREEN DRAGON",
+    "bio": "[B][C][32CD32]╭┈┈┈┈┈┈⎾ GREEN DRAGON ⏋┈┈┈┈┈┈╮\n[7CFC00]│ HARI AAG SE JALTA HAI HAR KOI MERI │\n[32CD32]╰┈┈┈┈┈┈⎾ ZINDAGI SE UDAAS HOJAEGA SAMNE ⎯⎯╯"
+  },
+  {
+    "name": "SILENT STORM",
+    "bio": "[I][00CED1]⎯⎯ SILENT STORM ⎯⎯\n[E0FFFF]AWAZ NAHI AAYEGI BAS GIRTE RAHENGE\n[00CED1]⎯⎯ TOOFAAN SE PEHLE JAISI CHUP SAMAJH LE ⎯⎯"
+  },
+  {
+    "name": "VELVET HEART",
+    "bio": "[C][FFB6C1]♡ ♡ ♡ VELVET HEART ♡ ♡ ♡\n[FF69B4]DIL MALIK KA LEKIN HAATH KHATARNAAK\n[FFB6C1]♡ GOLI SE DOSTI EMOTION SE NAHI KABHI ♡"
+  },
+  {
+    "name": "AQUA SHARK",
+    "bio": "[B][I][40E0D0]⌜⎾ AQUA SHARK ⏋⌝\n[7FFFD4]│ PANI MEIN JAISE SHER WAISE MEIN GAME MEIN │\n[40E0D0]⌞⎾ KHOON SOONGH KE POHOCH JAATA HUUN TAK ⎯⌟"
+  },
+  {
+    "name": "PURPLE HAZE",
+    "bio": "[I][9370DB]▁▂▃▄ PURPLE HAZE ▄▃▂▁\n[BA55D3]BADALO MEIN CHUPA BAITHA HUUN NICHOD DUNGA\n[9370DB]▔▁▂ DUSMAN SOCHTA RAHEGA MAIN KAHAN HUUN ▂▁▔"
+  },
+  {
+    "name": "CRYSTAL KING",
+    "bio": "[B][C][00FFFF]◊ ◊ ◊ CRYSTAL KING ◊ ◊ ◊\n[E0FFFF]SAAF DIKHTA HUUN LEKIN PAKDA NAHI JAATA\n[00FFFF]◊ TUTEGA TERA GAME JAISE SHEESHA CHAKNACHUR ◊"
+  },
+  {
+    "name": "BLAZING FIRE",
+    "bio": "[I][FF4500]⌈⌉⌈⌉ BLAZING FIRE ⌉⌈⌉⌈\n[FF8C00]AAG MEIN KHELTA HUUN JALTA NAHI MAIN\n[FF4500]⌊⌋⌊⌋ TERI TOH JALEDENGE EK HEADSHOT MEIN ⌋⌊⌋⌊"
+  },
+  {
+    "name": "STEEL SOUL",
+    "bio": "[B][C][A9A9A9]┏━━━━━━━━━━━━━━━━┓\n[C0C0C0]│ LOHE KA DIL HAI DARD NAHI HOTA │\n[A9A9A9]┗━━━━━━━━━━━━━━━━┛"
+  },
+  {
+    "name": "DARK WOLF",
+    "bio": "[I][2F4F4F]╱╲ DARK WOLF ╱╲\n[696969]ANDHERO MEIN BHOKTA HUUN KHOOON KE LIYE\n[2F4F4F]╲╱ CHILLAKE BULA LEKOIN KOI BACH NAHI PAYEGA ╲╱"
+  },
+  {
+    "name": "MAGMA KING",
+    "bio": "[C][FF4500]~~~~~ MAGMA KING ~~~~~\n[FF6347]ZAMEEN KE ANDAR SE AAYA HUUN JALANE TERI\n[FF4500]~~~~~ TAPKEGA PIGHLA KA LAWA TERI LASH PE ~~~~~"
+  },
+  {
+    "name": "NIGHT FURY",
+    "bio": "[B][I][191970]┌───⎾ NIGHT FURY ⏋───┐\n[483D8B]│ RAT KE 3 BAJE KA DARR DEKHA HAI │\n[191970]└───⎾ AAKHRI SAANS TAK PIYADA RAHEGA ───┘"
+  },
+  {
+    "name": "SOUL SNIPER",
+    "bio": "[I][00FF7F]⌜⌝ SOUL SNIPER ⌜⌝\n[7CFC00]│ DOOR SE DEKHA DIL MEIN GOLI MAARI │\n[00FF7F]⌞⌟ ROOH NIKALTI HAI AWAZ NAHI AATI BAS ┘"
+  },
+  {
+    "name": "CELESTIAL",
+    "bio": "[B][C][FFD700]✧ CELESTIAL DEATH ✧\n[FF69B4]AASMAN SE GIRA HUUN MAUT BANKAR\n[FFD700]✧ TARO SE BHI OOPAR HUUN RANK MEIN BHI ✧"
+  },
+  {
+    "name": "BLOOD VIPER",
+    "bio": "[I][B22222]╭━━━━━━━━━━━╮\n[DC143C]│ KHOON KA PYASA SAANP │\n[B22222]╰━━━━━━━━━━━╯"
+  },
+  {
+    "name": "ICE PHOENIX",
+    "bio": "[C][87CEEB]⎯⎯ ICE PHOENIX ⎯⎯\n[E0FFFF]BARF SE BANA LEKIN JINDA HUUN MARTA NAHI\n[87CEEB]⎯⎯ JALTA NAHI JAMATA HUUN SABKO ⎯⎯"
+  },
+  {
+    "name": "ROGUE SOUL",
+    "bio": "[B][I][808080]⌈⌈⌈ ROGUE SOUL ⌋⌋⌋\n[C0C0C0]BAGHI HUUN RULES SE NAHI BANDHATA\n[808080]⌊⌊⌊ MARTA HUUN APNE TAREEKE SE SAMAJH LE ⌈⌈⌈"
+  },
+  {
+    "name": "THUNDER GOD",
+    "bio": "[I][FFD700]◤◢◣◥ THUNDER GOD ◤◢◣◥\n[FFA500]GARJTA HUUN GIRTA NAHI BARSHTA HUUN\n[FFD700]◣◥◤◢ KADAK SE MAARTA HUUN SIRF EK SHOT ◣◥◤◢"
+  },
+  {
+    "name": "HONEY BADGER",
+    "bio": "[B][C][8B4513]╱╲ HONEY BADGER ╲╱\n[CD853F]DUNIYA KA SABSE BEWAKOOF JANWAR LEKIN KHATARNAAK\n[8B4513]╲╱ CHOTTA HUUN LEKIN SHER SE BHI JYADA DANGER ╱╲"
+  },
+  {
+    "name": "COSMIC VOID",
+    "bio": "[I][4B0082]┌─┐ COSMIC VOID ┌─┐\n[8A2BE2]│ ANDHERA JO KABHI KHATAM NAHI HOTA │\n[4B0082]└─┘ JAHA JAATA HUUN WAHAN UJAALA MITTA DETA DUUN └─┘"
+  },
+  {
+    "name": "VENOMOUS",
+    "bio": "[C][00FF00]⌜⎾ VENOMOUS ⏋⌝\n[ADFF2F]ZAHAR HILTA HAI KHOON MEIN GHULTA HAI\n[00FF00]⌞⎾ DUSMAN SOCHTA HAI MEIN KAHA HUUN LEKIN DEATH AAGAYI ⎯⌟"
+  },
+  {
+    "name": "SAND PRINCE",
+    "bio": "[B][I][F4A460]~~~~~ SAND PRINCE ~~~~~\n[FFD700]REGISTAN MEIN PALA HUUN TUFANO SE LADAA\n[F4A460]~~~~~ RET MEIN DUBA DETA HUUN DUSHMANO KO ~~~~~"
+  },
+  {
+    "name": "FIREFLY",
+    "bio": "[I][FFFF00]✧✧✧ FIREFLY ✧✧✧\n[FFD700]CHOTA HUUN LEKIN RAT MEIN NAZAR AATA HUUN\n[FFFF00]✧ CHAMAKTA HUUN MARTA HUUN BHAGTA HUUN ✧"
+  },
+  {
+    "name": "DARK PHOENIX",
+    "bio": "[B][C][8B008B]⎯⎯⎯ DARK PHOENIX ⎯⎯⎯\n[DA70D6]MARTA HUUN JITA HUUN MITTA NAHI KABHI\n[8B008B]⎯⎯ RAKH SE UTHTA HUUN AAG MEIN KHILTA HUUN ⎯⎯"
+  },
+  {
+    "name": "SILENT VIPER",
+    "bio": "[I][2E8B57]╭⌈⌉ SILENT VIPER ⌈⌉╮\n[3CB371]│ RENGTA HUUN DASSTA HUUN PATA NAHI CHALTA │\n[2E8B57]╰⌊⌋ GHAS MEIN SONA TERA MAUT SE BOLI SAMJH ⌋⌊⌋╯"
+  },
+  {
+    "name": "ROYAL GHOST",
+    "bio": "[C][FFD700]◊ ── ROYAL GHOST ── ◊\n[FF69B4]RAJA HUUN LEKIN DIKHTA NAHI MAARTA ZARUR\n[FFD700]◊ TAAJ PEHNA HUUN AUR KAFAN BHI PEHNA HUA ◊"
+  },
+  {
+    "name": "STORM SHADOW",
+    "bio": "[B][I][4682B4]▀▄▀ STORM SHADOW ▄▀▄\n[87CEFA]TOOFAAN KE SAATH AATA HUUN ANDHERO CHHODTA\n[4682B4]▄▀▄ CHUP REHTA HUUN BAS LAST MEIN MURGA BANATE DEKH ▀▄▀"
+  },
+  {
+    "name": "ELECTRIC VIPER",
+    "bio": "[I][FFD700]⌈⎾ ELECTRIC VIPER ⏋⌉\n[FFA500]│ 220 VOLT KA ZAHAR DETA HUUN DUSHMANO KO │\n[FFD700]⌊⎾ CURRENT LAGEGA RONE LAGEGA JALNE LAGEGA ⏋⌋"
+  },
+  {
+    "name": "NEBULA KING",
+    "bio": "[B][C][9370DB]~~~~ NEBULA KING ~~~~\n[BA55D3]SITARO KE BEECH MEIN CHUPA BAITHA HUUN\n[9370DB]~~~~ DUNIYA SE KAHA DURI HUUN PHIR BHI TOP MEIN HUUN ~~~~"
+  },
+  {
+    "name": "HELLFIRE",
+    "bio": "[I][B22222]◤◢◣◥ HELLFIRE ◤◢◣◥\n[FF4500]NARAK SE AAYA HUUN MAUT BAANTNE DUNIYA MEIN\n[B22222]◣◥◤◢ JALEGA SAB KUCH PICHE NAHI HATUNGA MAIN KABHI ◣◥◤◢"
+  },
+  {
+    "name": "PHANTOM STRIKE",
+    "bio": "[C][00CED1]╱╲╱ PHANTOM STRIKE ╲╱╲\n[E0FFFF]PATA NAHI CHALTA KAB KAHAN MARA MAIN NE\n[00CED1]╲╱╲ AAKHRI SAANS MEIN BHI DUSHMAN SOCHTA RAHEGA ╱╲╱"
+  },
+  {
+    "name": "SOUL REAPER",
+    "bio": "[B][I][DC143C]┌┐ SOUL REAPER ┌┐\n[FF69B4]│ ROOH NIKAL LETA HUUN BINA DARD DIYE │\n[DC143C]└┘ HASTA HUUN LASHO KE UPPER KHADAA HOON JAB MAIN └┘"
+  },
+  {
+    "name": "GOLDEN VIPER",
+    "bio": "[I][FFD700]⌜⌜⌜ GOLDEN VIPER ⌟⌟⌟\n[FFA500]SONE JAISA CHAMAKTA HUUN ZAHER JAISA MARTA\n[FFD700]⌞⌞⌞ DUNIYA CHAMAK DEKHE LEKIN KATIL HUUN MAIN ⌝⌝⌝"
+  }
+]
+}
